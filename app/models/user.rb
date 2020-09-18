61bbf4e85448f8f99a606cd13d9e385e376d2c8c
ryunosuketheend <ryu.always.onemu@gmail.com>
2020-09-18 21:09:07 +0900

    新規登録とログイン機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :password, length: { minimum: 6 }
     
-    VALID_PASSWORD_REGEX =/\A[A-Z0-9]+\z/
+    VALID_PASSWORD_REGEX =/\A[a-zA-Z0-9]+\z/
     validates :password, 
               format: { with: VALID_PASSWORD_REGEX,
-               message: "は半角6文字以上英小文字・数字それぞれ１文字以上含む必要があります"}
+               message: "は半角6文字以上英・大文字・小文字・数字それぞれ１文字以上含む必要があります"}
     validates :nickname, :birthday, :last_name, :last_name_katakana, :first_name, :first_name_katakana,  presence: true
     
     VALID_PASSWORD_REGEX =/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
