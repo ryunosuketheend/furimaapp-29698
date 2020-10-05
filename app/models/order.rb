d6792e9cd2556b380bf546cefd6e518ce303e089
ryunosuketheend <ryu.always.onemu@gmail.com>
2020-10-05 20:03:07 +0900

    商品購入機能修正2

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,4 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :address
-  # attr_accessor :token
-  # validates :price, presence: true
-  # validates :token, presence: true
 end
